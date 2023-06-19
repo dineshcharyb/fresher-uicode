150e688d1de571a1288e365663e0cd0c19dc9f28
vasantha122 <vasudappu24@gmail.com>
2023-06-19 19:38:03 +0530

    tets

---
+++ sign.js
@@ -0,0 +1,55 @@
+var select = document.getElementById("inputCountry");
+var options = [
+	"Afghanistan",
+	"Albania",
+	"Algeria",
+	"American Samoa",
+	"Andorra",
+	"Angola",
+	"Anguilla",
+	"Antarctica",
+	"Antigua and Barbuda",
+	"Argentina",
+	"Armenia",
+	"Aruba",
+	"Australia",
+	"Austria",
+	"Azerbaijan",
+	"Bahamas (the)",
+	"Bahrain",
+];
+options.forEach(function(opt){
+    var el=document.createElement("option");
+    el.textContent=opt;
+    el.value=opt;
+    select.appendChild(el);
+});
+
+var select = document.getElementById("inputState");
+var options = [
+	"Telangana",
+	"Andhra Pradesh",
+	"Assam",
+	"Kerala",
+	"Gujarath",
+	"Bihar",
+];
+options.forEach(function(opt){
+    var el=document.createElement("option");
+    el.textContent=opt;
+    el.value=opt;
+    select.appendChild(el);
+});
+
+
+
+
+
+const today=new Date();
+const yyyy = today.getFullYear();
+let mm=today.getMonth() +1;
+let dd =today.getDate();
+if(dd < 10) dd='0'+dd;
+if(mm < 10)mm='0'+mm;
+const Todaydate = dd+'/'+mm+'/'+yyyy;
+document.getElementById('Date').value=Todaydate;
