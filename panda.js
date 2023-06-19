35ed7e2129524aacde031e1420f58ad8fdda0d00
babumrcet1234 <108236318+babumrcet1234@users.noreply.github.com>
2023-06-19 19:10:21 +0530

    test

---
+++ panda.js
@@ -0,0 +1,62 @@
+const today = new Date();
+const yyyy =today.getFullYear();
+let mm = today.getMonth() +1;
+let dd = today.getDate();
+if(dd <10)dd ='0'+dd;
+if(mm <10)mm ='0'+mm;
+const Todaydate = dd+ '/'+ mm + '/'+yyyy;
+document.getElementById('startDate').value=Todaydate;
+
+var select = document.getElementById("inputCountry");
+var options = [
+	"Afghanistan",
+	"Albania",
+	"Algeria",
+	"American Samoa",
+	"Andorra",
+	"Angola",
+	"Anguilla",
+	"Antarctica",
+	"Antigua and Barbuda",
+	"Argentina",
+	"Armenia",
+	"Aruba",
+	"Australia",
+	"Austria",
+	"Azerbaijan",
+	"Bahamas (the)",
+	"Bahrain",
+];
+options.forEach(function(opt){
+    var el=document.createElement("option");
+    el.textContent=opt;
+    el.value=opt;
+    select.appendChild(el);
+});
+var select = document.getElementById("inputState");
+var options = [
+	"Afghanistan",
+	"Albania",
+	"Algeria",
+	"American Samoa",
+	"Andorra",
+	"Angola",
+	"Anguilla",
+	"Antarctica",
+	"Antigua and Barbuda",
+	"Argentina",
+	"Armenia",
+	"Aruba",
+	"Australia",
+	"Austria",
+	"Azerbaijan",
+	"Bahamas (the)",
+	"Bahrain",
+];
+options.forEach(function(opt){
+    var el=document.createElement("option");
+    el.textContent=opt;
+    el.value=opt;
+    select.appendChild(el);
+});
+
