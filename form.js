2b9a987e50fbee99ba5f4cddbf42de3d1a3fbcca
Umesh Chandra <umesh.kariveda@prutech.local>
2023-06-19 18:57:14 +0530

    test

---
+++ form.js
@@ -0,0 +1,31 @@
+const today = new Date();
+const yyyy = today.getFullYear();
+let mm = today.getMonth() + 1;
+let dd = today.getDate();
+if (dd < 10) dd = '0' + dd;
+if (mm < 10) mm = '0' + mm;
+const Todaydate = dd + '/' + mm + '/' + yyyy;
+document.getElementById('inputdate').value = Todaydate;
+
+
+var select = document.getElementById("inputCity");
+var options = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "American Samoa",
+  "Andorra",
+  "Angola",
+  "Anguilla",
+  "Antarctica",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Aruba",
+];
+options.forEach(function (opt) {
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+  });
